402350d0733349c578f972eed7ec36f57bebb1b5
Elena Pelaez <elenapelaez12@gmail.com>
2021-07-12 01:46:57 +0000

    add headers for mentors and mentees

--- portfolio/src/main/webapp/js/resources.js
+++ portfolio/src/main/webapp/js/resources.js
@@ -1,13 +1,42 @@
-//import * as jquery from "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
 
 var elem = document.querySelector('.sidenav');
 var instance = new M.Sidenav(elem);
 
 var el = document.querySelectorAll('.tab');
-//var instanceTabs = M.Tabs.init(el);
-   // with jquery
 
+// loads Materialize sidebar and tabs
   $(document).ready(function(){
    $('.sidenav').sidenav();
    $('.tabs').tabs();
   });
+
+// Make list of resources using Resources class
+
+/**
+ * Gets the list of Resources
+ */
+function loadResources() {
+    var resourcesList = [];
+    const resourceListElement = document.getElementById('resources-list');
+    resourcesList.forEach(function(element) {
+        resourcesListElement.appendChild(createResourceElement(element));
+    });
+}
+
+/**
+ * Prints a Resource to the HTML page inside a Materialize card
+ * @param resource an element containing info about a resource
+ */
+function createResourceElement(resource) {
+    const resourceElement = document.createElement('div');
+    const resourceTitle = document.createElement('span');
+    const resourceInfo = document.createElement('p');
+
+    resourceTitle.innerHTML = resource.getTitle;
+    resourceInfo.innerHTML = resource.getDescription;
+
+    resourceElement.appendChild(resourceTitle);
+    resourceElement.appendChild(resourceInfo);
+
+    return resourceElement;
+}
